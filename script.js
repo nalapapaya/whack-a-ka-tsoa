938928471a04bf06ce5aad7ebbfe004d1d479ec1
nalapapaya <madeline_lim_sy@hotmail.com>
2025-07-15 20:00:50 +0800

    added startover buttons

--- script.js
+++ script.js
@@ -48,11 +48,44 @@ function countdown() {
     endGame();
   }
 }
-
-//updating score
+//updating score upon hit
 function updateScore() {
   document.querySelector("#scoreDisplay").innerText = `Score: ${score}`;
 }
+
+//start game
+function startGame() {
+  score = 0;
+  updateScore();
+  hideAllPopups();
+
+  if (selectedMode === "hard") {
+    countdown(); //start timer
+  } else {
+    document.querySelector("#timer").innerText = "Timer: ∞";
+  }
+
+  //spawnRoaches()
+}
+
+//restart game - choose from mode
+function restartGame() {
+  document.querySelector("#scoreDisplay").innerHTML = `&nbsp;`;
+  document.querySelector("#timer").innerHTML = `&nbsp;`;
+  score = 0;
+  currentPopup = 3;
+  showPopup(currentPopup);
+}
+
+//reset game - from player name
+function startOver() {
+  document.querySelector("#scoreDisplay").innerHTML = `&nbsp;`;
+  document.querySelector("#timer").innerHTML = `&nbsp;`;
+  score = 0;
+  currentPopup = 1;
+  showPopup(currentPopup);
+}
+
 /*----------------------------- Event Listeners -----------------------------*/
 //show 1st popup on page
 window.addEventListener("load", () => {
@@ -105,10 +138,19 @@ document.querySelector("#god").addEventListener("click", () => {
 });
 
 //start timer for hard mode
-document.querySelector("#startGame").addEventListener("click", () => {
-  if (selectedMode === "hard") {
-    countdown();
-  } else {
-    document.querySelector("#timer").innerText = "Timer: ∞";
-  }
-});
+// document.querySelector("#startGame").addEventListener("click", () => {
+//   if (selectedMode === "hard") {
+//     countdown();
+//   } else {
+//     document.querySelector("#timer").innerText = "Timer: ∞";
+//   }
+// });
+
+//start game
+document.querySelector("#startGame").addEventListener("click", startGame);
+
+//restart game
+document.querySelector("#restartGame").addEventListener("click", restartGame);
+
+//reset game
+document.querySelector("#resetGame").addEventListener("click", startOver);
