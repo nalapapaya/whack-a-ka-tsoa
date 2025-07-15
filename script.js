1b7b41df6e600492d6225d4d32c3f5a5cc19af93
nalapapaya <madeline_lim_sy@hotmail.com>
2025-07-15 19:31:16 +0800

    added timer,player name & score

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
-const totalPopups = 4; //excludes results popup
+const totalPopups = 5; //excludes results popup
 
 /*---------------------------- Variables (state) ----------------------------*/
 let currentPopup = 1;
-let score;
+let score = 0;
+let timeLeft;
+let selectedMode = null;
 
 /*------------------------ Cached Element References ------------------------*/
 const imageWrapper = document.querySelector(".img-placeholder");
 const popupEls = document.querySelectorAll(".popup");
 const allButtons = document.querySelectorAll(".button");
 const closeButtons = document.querySelectorAll(".popupclose");
+const next = document.querySelector("#next");
 
 /*-------------------------------- Functions --------------------------------*/
 //Showing popups by steps
@@ -31,7 +34,24 @@ function hideAllPopups() {
 function endGame() {
   const results = document.querySelector("#resultMessage");
   results.innerText = `You eliminated ${score} ka-tsoa(s)!`;
-  showPopup(5);
+  showPopup(6);
+}
+
+//hard mode timeleft
+function countdown() {
+  if (timeLeft > 0) {
+    document.querySelector("#timer").innerText = `Timer: ${timeLeft}s`;
+    timeLeft--;
+    setTimeout(countdown, 1000); //call countdown in 1s
+  } else {
+    document.querySelector("#timer").innerText = `Time's up!`;
+    endGame();
+  }
+}
+
+//updating score
+function updateScore() {
+  document.querySelector("#scoreDisplay").innerText = `Score: ${score}`;
 }
 /*----------------------------- Event Listeners -----------------------------*/
 //show 1st popup on page
@@ -45,7 +65,7 @@ allButtons.forEach((button) => {
     if (currentPopup < totalPopups) {
       currentPopup++;
       showPopup(currentPopup);
-    } 
+    }
     // else if (currentPopup === totalPopups) {
     //   showPopup(currentPopup);
     // }
@@ -53,9 +73,42 @@ allButtons.forEach((button) => {
   });
 });
 
+//for last popup
 closeButtons.forEach((button) => {
   button.addEventListener("click", () => {
     popupEls.forEach((p) => (p.style.display = "none"));
     imageWrapper.classList.remove("blur");
-  })
-})
+  });
+});
+
+//for player name input
+next.addEventListener("click", () => {
+  const inputName = document.querySelector("#playerInput").value;
+  document.querySelector("#nameDisplay").innerText =
+    `Player:  ${inputName}` || `Player: Guest`;
+});
+
+//player selects mode
+document.querySelector("#hard").addEventListener("click", () => {
+  selectedMode = "hard";
+  timeLeft = 5; //reset timer
+});
+
+document.querySelector("#easy").addEventListener("click", () => {
+  selectedMode = "easy";
+  timeLeft = Infinity; //no timer
+});
+
+document.querySelector("#god").addEventListener("click", () => {
+  selectedMode = "god";
+  timeLeft = Infinity; //no timer
+});
+
+//start timer for hard mode
+document.querySelector("#startGame").addEventListener("click", () => {
+  if (selectedMode === "hard") {
+    countdown();
+  } else {
+    document.querySelector("#timer").innerText = "Timer: ∞";
+  }
+});
