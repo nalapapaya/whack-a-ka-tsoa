f4ff3860b59851da7140c5052c93b0df7c165602
nalapapaya <madeline_lim_sy@hotmail.com>
2025-07-16 18:53:51 +0800

    completed hardmode

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let selectedMode = null;
 let totalHardRoach = 100; //maximum spawn for hard mode
 let spawnTimeout; //cancel roach spawn when game stops
 let countdownTimeout; //stop countdown early
+let hardTimeout = []; //global const for hard mode for different spawn time
 
 /*------------------------ Cached Element References ------------------------*/
 const imageWrapper = document.querySelector(".img-placeholder");
@@ -19,6 +20,9 @@ const next = document.querySelector("#next");
 const easyRoach = document.querySelector("#easyRoach");
 const godRoach = document.querySelector("#godRoach");
 const imgPlaceholder = document.querySelector(".img-placeholder");
+const hardRoach1 = document.querySelector("#hardRoach");
+const hardRoach2 = document.querySelector("#hardRoach2");
+const hardRoach3 = document.querySelector("#hardRoach3");
 /*-------------------------------- Functions --------------------------------*/
 //Showing popups by steps
 function showPopup(step) {
@@ -39,10 +43,11 @@ function hideAllPopups() {
 function endGame() {
   const results = document.querySelector("#resultMessage");
   results.innerText = `You eliminated ${score} ka-tsoa(s)!`;
+  hardTimeout.forEach(clearTimeout)
   showPopup(6);
 }
 
-//winning conditiobn - easy mode 
+//winning conditiobn - easy mode
 function endEasyGame() {
   const results = document.querySelector("#resultMessage");
   results.innerText = "You got the ka-tsoa!";
@@ -53,15 +58,16 @@ function endEasyGame() {
 function tooEarly() {
   hideAllPopups();
   document.querySelector("#popup7").style.display = "block";
-  imgPlaceholder.classList.add('blur');
+  imgPlaceholder.classList.add("blur");
 }
 
 //losing condition - too late (easy mode)
 function tooLate() {
   hideAllPopups();
   document.querySelector("#popup7").style.display = "block";
-  document.querySelector("#missedMessage").innerText = "You missed it! The ka-tsoa got you!"
-  imgPlaceholder.classList.add('blur');
+  document.querySelector("#missedMessage").innerText =
+    "You missed it! The ka-tsoa got you!";
+  imgPlaceholder.classList.add("blur");
 }
 
 //hard mode timeleft
@@ -91,21 +97,24 @@ function startGame() {
   if (selectedMode === "hard") {
     countdown(); //start timer
     document.querySelector(".hardBox").style.display = "block";
+    document.querySelector(".hardBox2").style.display = "block";
+    document.querySelector(".hardBox3").style.display = "block";
   } else if (selectedMode === "easy") {
     document.querySelector("#timer").innerText = "Timer: \u221E";
     document.querySelector(".easyBox").style.display = "block";
   } else if (selectedMode === "god") {
     document.querySelector("#timer").innerText = "Timer: \u221E";
-    document.querySelector(".easyBox").style.display = "block";
+    document.querySelector(".godBox").style.display = "block";
   }
   spawnRoaches();
 }
 
-//spawn roaches - hard mode
+//spawn roaches
 function spawnRoaches() {
   if (selectedMode === "hard") {
-    //add random timer for spawn hard mode
-    document.querySelector("#hardRoach").style.display = "block";
+    spawnHardRoach(hardRoach1);
+    spawnHardRoach(hardRoach2);
+    spawnHardRoach(hardRoach3);
   } else if (selectedMode === "easy") {
     spawnEasyRoach();
   } else if (selectedMode === "god") {
@@ -123,24 +132,56 @@ function spawnEasyRoach() {
 }
 //spawn god roach
 function spawnGodRoach() {
-  const delay = Math.floor(Math.random() * 5000); //up to 3s delay
+  const delay = Math.floor(Math.random() * 3000); //up to 3s delay
   spawnTimeout = setTimeout(() => {
     godRoach.style.display = "block";
-    godRoach.style.pointerEvents = "auto"; 
+    godRoach.style.pointerEvents = "auto";
   }, delay);
+}
+
+//reset animation for respawn
+function resetAnimation(element) {
+  element.classList.remove("animate");
+  element.classList.add("animate");
+}
+
+//spawn hard roach
+function spawnHardRoach(cockroach, minDelay = 1000, maxDelay = 3000) {
+  //to spawn between 1 to 3s delay
+  const delay = Math.random() * (maxDelay - minDelay + 1) + minDelay; //inclusive min delay & +1 to include max delay
+  const timeout1 = setTimeout(() => {
+    //for spawning
+    cockroach.style.display = "block";
+    cockroach.style.pointerEvents = "auto";
+    resetAnimation(cockroach);
+    const timeout2 = setTimeout(() => {
+      //for disappearing
+      cockroach.style.display = "none";
+      cockroach.style.pointerEvents = "none";
+      spawnHardRoach(cockroach, minDelay, maxDelay); //allow to spawn again
+    }, 3000); //disappears in 3s
+    hardTimeout.push(timeout2);
+  }, delay);
+  hardTimeout.push(timeout1);
 }
 
 //restart game - choose from mode
 function restartGame() {
-  clearTimeout(spawnTimeout); //clear spawn
-  clearTimeout(countdownTimeout); //clear timer
+  clearTimeout(spawnTimeout); //clear spawn easy & god mode
+  clearTimeout(countdownTimeout); //clear timer easy & god mode
+  hardTimeout.forEach(clearTimeout) //clear spawn hard mode
   document.querySelector("#scoreDisplay").innerHTML = `&nbsp;`;
   document.querySelector("#timer").innerHTML = `&nbsp;`;
   document.querySelector(".easyBox").style.display = "none";
+  document.querySelector(".godBox").style.display = "none";
   document.querySelector(".hardBox").style.display = "none";
-  document.querySelector("#easyRoach").style.display = "none";
-  document.querySelector("#hardRoach").style.display = "none";
-  document.querySelector("#godRoach").style.display = "none";
+  document.querySelector(".hardBox2").style.display = "none";
+  document.querySelector(".hardBox3").style.display = "none";
+  easyRoach.style.display = "none";
+  hardRoach1.style.display = "none";
+  hardRoach2.style.display = "none";
+  hardRoach3.style.display = "none";
+  godRoach.style.display = "none";
   selectedMode = null;
   score = 0;
   currentPopup = 3;
@@ -151,14 +192,20 @@ function restartGame() {
 function startOver() {
   clearTimeout(spawnTimeout); //clear spawn
   clearTimeout(countdownTimeout); //clear timer
+  hardTimeout.forEach(clearTimeout) //clear spawn hard mode
   document.querySelector("#scoreDisplay").innerHTML = `&nbsp;`;
   document.querySelector("#timer").innerHTML = `&nbsp;`;
   document.querySelector("#nameDisplay").innerHTML = `&nbsp;`;
   document.querySelector(".easyBox").style.display = "none";
+  document.querySelector(".godBox").style.display = "none";
   document.querySelector(".hardBox").style.display = "none";
-  document.querySelector("#easyRoach").style.display = "none";
-  document.querySelector("#hardRoach").style.display = "none";
-  document.querySelector("#godRoach").style.display = "none";
+  document.querySelector(".hardBox2").style.display = "none";
+  document.querySelector(".hardBox3").style.display = "none";
+  easyRoach.style.display = "none";
+  hardRoach1.style.display = "none";
+  hardRoach2.style.display = "none";
+  hardRoach3.style.display = "none";
+  godRoach.style.display = "none";
   score = 0;
   currentPopup = 1;
   showPopup(currentPopup);
@@ -236,7 +283,7 @@ document.querySelector("#god").addEventListener("click", () => {
 document.querySelector("#startGame").addEventListener("click", startGame);
 
 //restart game (all modes)
-document.querySelectorAll(".restartGameBtn").forEach(btn => {
+document.querySelectorAll(".restartGameBtn").forEach((btn) => {
   btn.addEventListener("click", restartGame);
 });
 
@@ -252,31 +299,43 @@ easyRoach.addEventListener("animationend", () => {
 
 //disappear easyRoach - losing condition (clicked too early)
 imgPlaceholder.addEventListener("click", (event) => {
-  const ifRoachVisible = easyRoach.style.display === 'block' && easyRoach.style.pointerEvents !== "none"; //correct condition for win
+  const ifRoachVisible =
+    easyRoach.style.display === "block" &&
+    easyRoach.style.pointerEvents !== "none"; //correct condition for win
   const notRoach = event.target === easyRoach; //click was not easyRoach
   if (!ifRoachVisible && !notRoach && selectedMode === "easy") {
     tooEarly();
-  } else if (ifRoachVisible && !notRoach) { //did not click on roach after it spawns
+  } else if (ifRoachVisible && !notRoach) {
+    //did not click on roach after it spawns
     tooLate();
   }
-})
+});
 
 //disappear easyRoach - winning condition
-easyRoach.addEventListener("click", ()=> {
-  easyRoach.style.pointerEvents = "none"; 
-  easyRoach.style.display = "none"; 
+easyRoach.addEventListener("click", () => {
+  easyRoach.style.pointerEvents = "none";
+  easyRoach.style.display = "none";
   score++;
   updateScore();
   endEasyGame();
 });
 
-//
-
 //disappear godRoach - winning condition
-godRoach.addEventListener("click", ()=> {
-  godRoach.style.pointerEvents = "none"; 
-  godRoach.style.display = "none"; 
+godRoach.addEventListener("click", () => {
+  godRoach.style.pointerEvents = "none";
+  godRoach.style.display = "none";
   score++;
   updateScore();
   endEasyGame();
+});
+
+//scoring for hard mode
+const allHardRoaches = [hardRoach1, hardRoach2, hardRoach3];
+allHardRoaches.forEach((cockroach) => {
+  cockroach.addEventListener("click", () => {
+    cockroach.style.pointerEvents = "none";
+    cockroach.style.display = "none";
+    score++;
+    updateScore();
+  });
 });
